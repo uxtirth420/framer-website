89f0bb36af78c01e16f3b072825792ebdeff8e64
Tirth Patel <vasanitirth2900@gmail.com>
2026-03-23 21:56:44 +0530

    all-made

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('.btn1');
+let a = document.querySelector('.a');
+
+btn.addEventListener('click', function() {
+    btn.style.scale = '50%';
+    a.style.display = 'block'
+})
+
